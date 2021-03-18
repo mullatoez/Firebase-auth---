3380257e72e6dfe4e8ecfcd73693dc2a5d61d0ae
Mullatoez <kimanthikevin76@gmail.com>
2021-03-19 02:15:11 +0300

    starter code --

---
+++ app/src/main/java/com/mullatoez/firebaseauthemailpassword/DashBoardActivity.kt
@@ -0,0 +1,12 @@
+package com.mullatoez.firebaseauthemailpassword
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DashBoardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_dashboard)
+    }
+}
--- app/src/main/java/com/mullatoez/firebaseauthemailpassword/RegisterActivity.kt
+++ app/src/main/java/com/mullatoez/firebaseauthemailpassword/RegisterActivity.kt
@@ -2,10 +2,53 @@ package com.mullatoez.firebaseauthemailpassword
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.mullatoez.firebaseauthemailpassword.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonRegister.setOnClickListener {
+
+            performRegistration()
+        }
+    }
+
+    private fun performRegistration() {
+        val email = binding.emailEditText.text.toString()
+        val password = binding.passwordEditText.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(
+                this,
+                "All fields required", Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+
+        val auth = FirebaseAuth.getInstance()
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+
+                Toast.makeText(
+                    this,
+                    "Authentication was successful", Toast.LENGTH_SHORT
+                ).show()
+
+            }
+            .addOnFailureListener {
+                Toast.makeText(
+                    this,
+                    "Authentication Failed" + it.localizedMessage, Toast.LENGTH_SHORT
+                ).show()
+            }
     }
 }
